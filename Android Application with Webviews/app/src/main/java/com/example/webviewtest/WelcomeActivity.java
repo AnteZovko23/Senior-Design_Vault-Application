5f63ed946339d56a628545c25216a0cfd484b78b
Christopher Johnson <chrisxsonicwavex@gmail.com>
2022-04-27 12:48:56 -0500

    Animation at the start implemented

---
+++ Android Application with Webviews/app/src/main/java/com/example/webviewtest/WelcomeActivity.java
@@ -0,0 +1,43 @@
+package com.example.webviewtest;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class WelcomeActivity extends AppCompatActivity {
+    ImageView WelcomeView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+        WelcomeView = (ImageView) findViewById(R.id.WelcomeView);
+        Animation animation = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fadein);
+        WelcomeView.setAnimation(animation);
+
+        animation.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                finish();
+                startActivity(new Intent(getApplicationContext(), LoginActivity.class));
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });{
+        };
+
+    }
+
+}
