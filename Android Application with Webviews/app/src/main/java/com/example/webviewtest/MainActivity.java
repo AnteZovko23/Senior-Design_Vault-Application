6b7a38ea27aea20f780850d04d16f4c01123447e
xSonicWavex <chrisxsonicwavex@gmail.com>
2022-04-28 20:38:33 -0500

    successfully merged Jeremy's Bluetooth code with Chris's Design.

--- Android Application with Webviews/app/src/main/java/com/example/webviewtest/MainActivity.java
+++ Android Application with Webviews/app/src/main/java/com/example/webviewtest/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.webviewtest;
 
+import android.app.Notification;
+import android.app.PendingIntent;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.SystemClock;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -9,6 +13,13 @@ import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -21,6 +32,14 @@ public class MainActivity extends AppCompatActivity {
     private Button facebutton;
     private Button pickerbutton;
     private Toolbar toolbar;
+    private Notification notification;
+    NotificationThread t1;
+    public static NotificationCompat.Builder strangerNotification;
+    public static int strangerNotificationID = 101;
+    public static NotificationManagerCompat notificationManager;
+    private static String CHANNEL_ID = "vault_notifications";
+    private static boolean strangerDetected = false;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +47,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         toolbar=findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        t1 = new NotificationThread();
+        t1.start();
     }
 
     @Override
@@ -81,5 +102,50 @@ public class MainActivity extends AppCompatActivity {
     public void openPicker() {
         Intent intent = new Intent(this, PickImageActivity.class);
         startActivity(intent);
+    }
+
+    class NotificationThread extends Thread {
+        private SharedPreferences sharedPreferences;
+        private int userCountdownTime;
+        private String input = "A stranger has been detected.";
+
+        @Override
+        public void run() {
+            while (true) {
+                SystemClock.sleep(1000);
+                httpRequest();
+                System.out.println("yo");
+            }
+        }
+
+        public void httpRequest() {
+            String url = "http://192.168.1.5:5000/get_notification";
+            RequestQueue mRQueue;
+            StringRequest mSReq;
+            mRQueue = Volley.newRequestQueue(MainActivity.this);
+            mSReq = new StringRequest(Request.Method.GET, url, response -> {
+
+                if(response.equals("nothing")) {
+                    sendNotification();
+                }
+
+            }, error -> {});
+            mRQueue.add(mSReq);
+        }
+
+        private void sendNotification() {
+            Intent notificationIntent = new Intent(MainActivity.this, MainActivity.class);
+            notificationIntent.putExtra("stranger", true);
+            notificationIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            PendingIntent pendingIntent = PendingIntent.getBroadcast(MainActivity.this, 0, notificationIntent, PendingIntent.FLAG_MUTABLE);
+
+            strangerNotification = new NotificationCompat.Builder(MainActivity.this, CHANNEL_ID)
+                    .setContentTitle("The Vault")
+                    .setContentText(input)
+                    .setSmallIcon(R.drawable.smiley);
+
+            notificationManager = NotificationManagerCompat.from(MainActivity.this);
+            notificationManager.notify(strangerNotificationID, strangerNotification.build());
+        }
     }
 }
